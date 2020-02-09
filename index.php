0653f919fc6a19f59296669167e146f9a41d9741
sgtkaykay <60122578+sgtkaykay@users.noreply.github.com>
2020-02-09 15:15:40 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
 <?php
-  echo "FFFFFFFF";
+ $json = file_get_contents("https://jsonfaus.herokuapp.com/json.php");
+
+ $data = json_decode($json,true);
+ $list = $data['Books'];
 ?>
+
+<h1>Top 5 Music in 2020</h1>
+
+<table border="1px">
+  <tr class="head">
+    <td>Title</td>
+    <td>Artist</td>
+    <td>Genre</td>
+    <td>Writer</td>
+  </tr>
+<?php
+  foreach($list as $value){
+?>
+  <tr>
+    <td><?php echo $value['mus_title'];?></td>
+    <td><?php echo $value['mus_artist'];?></td>
+    <td><?php echo $value['mus_genre'];?></td>
+    <td><?php echo $value['mus_writer'];?></td>
+  </tr>
+<?php
+  }
+?>
+</table>
